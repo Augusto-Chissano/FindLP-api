da7b1584ae70b52f8550deffd64f6bd703d7a80c
Augusto <shuaiksakur@gmail.com>
2023-05-03 15:51:44 +0200

    First version going on

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 //
-//const user = "augustochissano";
-//const pass = encodeURIComponent("488688Guto");
+const user = "augustochissano";
+const pass = encodeURIComponent("488688Guto");
 //Localhost
-const db = () => mongoose.connect("mongodb://localhost/findlp");
+//const db = () => mongoose.connect("mongodb://localhost/findlp");
 //Cloud
-//const db = () => mongoose.connect(`mongodb+srv://${user}:${pass}@findlp.uik7ii3.mongodb.net/?retryWrites=true&w=majority`);
+const db = () => mongoose.connect(`mongodb+srv://${user}:${pass}@findlp.uik7ii3.mongodb.net/?retryWrites=true&w=majority`);
 
 module.exports = db;
